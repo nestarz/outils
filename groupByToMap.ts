204368685c29efe53a2a1fd04b167f654e2cfb74
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2023-08-07 16:05:24 +0200

    fix: groupByToMap

---
+++ groupByToMap.ts
@@ -0,0 +1,29 @@
+export function groupByToMap<T, K>(
+  array: T[],
+  callback: (arg: T) => K
+): Map<K, T[]> {
+  if (!Array.isArray(array))
+    throw new TypeError(`Can’t call method on ${array}`);
+  if (typeof callback !== "function")
+    throw new TypeError(`${callback} is not a function`);
+
+  const result = new Map<K, T[]>();
+  array.forEach((value: T) => {
+    const key: K = callback(value);
+    const collection: T[] = result.get(key) || [];
+    collection.push(value);
+    result.set(key, collection);
+  });
+
+  return result;
+}
+
+const preferNative: typeof groupByToMap = (array, callback) => {
+  // @ts-ignore: ponyfill Map.groupBy
+  if (Map.prototype.groupBy !== undefined)
+    // @ts-ignore: ponyfill Map.groupBy
+    return Map.prototype.groupBy(array, callback);
+  return groupByToMap(array, callback);
+};
+
+export default preferNative;
