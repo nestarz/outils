3e322d53cffde1977c8c650b2bcd222bdd11c5fa
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2023-08-12 22:19:33 +0200

    feat: kysely/json.ts

---
+++ kysely/json.ts
@@ -0,0 +1,11 @@
+import type { Expression, RawBuilder, Simplify } from "https​://esm.sh/kysely​@0.26.1?dts";
+
+export const jsonObjectFrom = <O>(
+  expr: Expression<O>
+): RawBuilder<Simplify<O> | null> =>
+  sql`(select to_json(obj) from ${expr} as obj)`;
+
+export const jsonAggToJsonObjectFrom = <O>(
+  expr: Expression<O>
+): RawBuilder<Simplify<O[]> | null> =>
+  sql`(select json_agg(to_json(obj)) from ${expr} as obj)`;
