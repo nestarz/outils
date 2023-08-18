5790d70d9d8c9ebde1f3b9b91575f243d744b7d5
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2023-08-18 17:13:26 +0200

    feat: generateGraphQLTypes.ts

---
+++ generateGraphQLTypes.ts
@@ -0,0 +1,97 @@
+import {
+  getIntrospectionQuery,
+  buildClientSchema,
+  printSchema,
+  parse,
+  type GraphQLSchema,
+} from "https://deno.land/x/graphql_deno@v15.0.0/mod.ts";
+
+const readOnly = !!Deno.env.get("DENO_DEPLOYMENT_ID");
+
+const createCache = () => {
+  const cacheMap = new Map();
+  return {
+    setItem: (key: string, value: any) => {
+      return globalThis.localStorage
+        ? globalThis.localStorage.setItem(String(key), JSON.stringify(value))
+        : cacheMap.set(key, value);
+    },
+    getItem: (key: string) => {
+      if (!globalThis.localStorage) return cacheMap.get(key);
+      const value = localStorage.getItem(String(key));
+      return value ? JSON.parse(value) : value;
+    },
+    has: (key: string) =>
+      globalThis.localStorage
+        ? localStorage.getItem(String(key)) !== null
+        : cacheMap.has(key),
+  };
+};
+
+export const createGetSchemaFromUrl = (
+  graphqlClient: (...args: any[]) => Promise<Response>
+) => {
+  return async () => {
+    console.time("[introspection]");
+    const response = await graphqlClient(getIntrospectionQuery());
+    const { data } = await response.json();
+    const graphqlSchemaObj = buildClientSchema(data);
+    console.timeEnd("[introspection]");
+    return graphqlSchemaObj;
+  };
+};
+
+export const createGetTypeScriptDef = (
+  getSchema: () => Promise<GraphQLSchema>
+) => {
+  const localCache = createCache();
+
+  return async () => {
+    if (readOnly) return;
+    const typescriptPlugin = await import(
+      "npm:@graphql-codegen/typescript@4.0.0"
+    );
+    const { codegen } = await import("npm:@graphql-codegen/core@4.0.0");
+
+    const schema = printSchema(await getSchema());
+    const cache = await (async () =>
+      await localCache.getItem(import.meta.url))().catch(() => null);
+    if (schema && schema === cache?.schema && cache?.typeScriptDefinition)
+      return cache.typeScriptDefinition;
+    else if (!schema) console.warn("No schema provided");
+    console.time("[codegen]");
+    const config = {
+      documents: [],
+      config: {},
+      schema: parse(schema),
+      plugins: [{ typescript: {} }],
+      pluginMap: {
+        typescript: typescriptPlugin,
+      },
+    };
+    const typeScriptDefinition = await codegen(config);
+    console.timeEnd("[codegen]");
+    localCache.setItem(import.meta.url, { typeScriptDefinition, schema });
+    return typeScriptDefinition;
+  };
+};
+
+export const createSaveDefToFile = (
+  getTypeScriptDef: ReturnType<typeof createGetTypeScriptDef>,
+  name = "types.d.ts"
+) => {
+  let cache: string;
+  return async () => {
+    const typeScriptDefinition = await getTypeScriptDef();
+    if (cache === typeScriptDefinition || readOnly) return;
+    const fn = [Deno.cwd(), name].join("/");
+    cache = typeScriptDefinition;
+    if ((await Deno.readTextFile(fn).catch(() => "")) !== typeScriptDefinition)
+      await Deno.writeTextFile(fn, typeScriptDefinition);
+  };
+};
+
+export const createTypes = (fetcher: (...args: any[]) => Promise<Response>) => () =>
+  createSaveDefToFile(
+    createGetTypeScriptDef(createGetSchemaFromUrl(fetcher))
+  )().catch(console.error);
