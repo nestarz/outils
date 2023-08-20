0b0c12feab19f4d00aeb858c61c8d039e7772fe5
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2023-08-20 12:47:58 +0200

    fix: staleWhileRevalidate.ts

--- staleWhileRevalidate.ts
+++ staleWhileRevalidate.ts
@@ -1,45 +1,62 @@
-type CacheItem = {
-  response: Response;
-  expiration: number;
-  updatePromise: null | Promise<any>;
-};
-const cache = new Map<string, CacheItem>();
-const isSameResponse = (response1: Response, response2: Response) => {
-  // Implement your comparison logic to check if two responses are the same
-  return (
-    response1.body === response2.body && response1.status === response2.status
-  );
-};
-type Callback = () => Response | Promise<Response>;
+import { createKv, streamToJson } from "./kvUtils.ts";
 
-const updateCache =
-  (revalidateAfter: number) =>
-  async (key: string, handler: Callback, cacheEntry: CacheItem | undefined) => {
-    const response = await handler();
-    if (response?.status === 200)
-      if (!cacheEntry || !isSameResponse(cacheEntry.response, response))
-        cache.set(key, {
-          response,
-          expiration: Date.now() + revalidateAfter * 1000,
-          updatePromise: null,
-        });
-    return response;
+const createCache = () => {
+  const cacheMap = new Map();
+  const kv = createKv(Deno.env.get("DENO_DEPLOYMENT_ID") ? undefined : "1");
+  const saveToLocal = (key: string, value: any) =>
+    cacheMap.set(String(key), JSON.stringify(value));
+  return {
+    setItem: async (key: string, value: any) => {
+      await kv?.saveFile(key, new TextEncoder().encode(JSON.stringify(value)));
+      return saveToLocal(key, value);
+    },
+    getItem: async (key: string) => {
+      const value =
+        cacheMap.get(key) ??
+        (await kv
+          ?.getFile(String(key))
+          .then((value) => (value ? streamToJson(value) : value))
+          .then((value) => (value ? (saveToLocal(key, value), value) : value)));
+      return typeof value === "string" ? JSON.parse(value) : value;
+    },
+    has: (key: string) => cacheMap.has(key),
   };
-
-const staleWhileRevalidate = async (
-  key: string,
-  handler: Callback,
-  revalidateAfter = 60 * 100
-) => {
-  const cacheEntry = cache.get(key);
-  const update = updateCache(revalidateAfter);
-  if (cacheEntry)
-    if (cacheEntry.expiration > Date.now()) {
-      cacheEntry.updatePromise ??= update(key, handler, cacheEntry);
-      return cacheEntry.response.clone();
-    }
-  const response = await update(key, handler, cacheEntry);
-  return response.clone();
 };
 
-export default staleWhileRevalidate;
+const cache = createCache();
+
+const jsonStringifyWithBigIntSupport = (data: unknown) => {
+  if (data !== undefined) {
+    return JSON.stringify(data, (_, v) =>
+      typeof v === "bigint" ? `${v}#bigint` : v
+    ).replace(/"(-?\d+)#bigint"/g, (_, a) => a);
+  }
+};
+
+const isFresh = (timestamp: number, maxAge: number) =>
+  Date.now() - timestamp < maxAge;
+
+export async function staleWhileRevalidate<U>(
+  key: string,
+  fetchFunc: (arg0: string) => Promise<U>,
+  maxAge: number
+) {
+  const value = await cache.getItem(key);
+  if (value) {
+    // && isFresh(cache.getItem(key).timestamp, maxAge)) {
+    if (!isFresh(value.timestamp, maxAge / 2))
+      fetchFunc(key).then((data: U) =>
+        cache.setItem(key, {
+          data: jsonStringifyWithBigIntSupport(data),
+          timestamp: Date.now(),
+        })
+      );
+    return JSON.parse(value.data) as U;
+  }
+  const data = await fetchFunc(key);
+  cache.setItem(key, {
+    data: jsonStringifyWithBigIntSupport(data),
+    timestamp: Date.now(),
+  });
+  return data as U;
+}
