af791435e225960e9cb4144d878ae1967cdc253d
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2023-08-22 11:51:58 +0200

    fix: sse.ts

--- sse.ts
+++ sse.ts
@@ -1,28 +1,80 @@
 export interface SSEContext {
-  sse: {
-    dispatch: (msg: string) => void;
-    controller: ReadableStreamDefaultController<any>;
-    readableStream: ReadableStream;
-  };
+  dispatch: (msg: string) => void;
+  streamsControllers: Map<
+    string,
+    {
+      controller: ReadableStreamDefaultController<unknown>;
+      readableStream: ReadableStream;
+      reuseKey: string;
+    }
+  >;
 }
+
+const createId = ((id = 0) => () => id++)(); // prettier-ignore
+
+const promiseCache = new Map<
+  string,
+  { sse: SSEContext; promise: Promise<unknown> }
+>();
 
 export const sse =
   <T, U, V>(
-    fn: (arg1: T, arg2: U & SSEContext) => V,
+    fn: (arg1: T, arg2: U & { sse: SSEContext }) => V,
     options?: {
       requestInit?: ResponseInit;
-      encode: (payload: unknown) => string;
+      encode?: (payload: unknown) => string;
+      reuseKeyFn?: (req: T, ctx: U) => string[] | Promise<string[]>;
     }
   ) =>
-  (req: T, ctx: U) => {
+  async (req: T, ctx: U) => {
+    const streamKey = createId().toString();
+    const reuseKey =
+      (await options?.reuseKeyFn?.(req, ctx))?.join(",") ?? streamKey;
+
+    const encode =
+      options?.encode ??
+      ((msg: string) => new TextEncoder().encode(`data: ${msg}\r\n\r\n`));
+    const createDispatch =
+      (streams: SSEContext["streamsControllers"]) => (msg: string) => {
+        streams.forEach(({ reuseKey: key, readableStream, controller }) => {
+          if (!readableStream.locked && key === reuseKey) {
+            try {
+              controller.enqueue(encode(msg));
+            } catch {
+              promiseCache
+                .get(reuseKey)
+                ?.sse.streamsControllers.delete(streamKey);
+            }
+          }
+        });
+      };
+
     const readableStream = new ReadableStream({
       start(controller) {
-        const encode =
-          options?.encode ??
-          ((msg: string) => new TextEncoder().encode(`data: ${msg}\r\n\r\n`));
-        const dispatch = (msg: string) => controller.enqueue(encode(msg));
-        const sse = { dispatch, controller, readableStream: this as ReadableStream };
-        fn(req, { ...ctx, sse }).then(() => controller.close());
+        if (!promiseCache.has(reuseKey)) {
+          const streamsControllers: SSEContext["streamsControllers"] =
+            new Map();
+          const sse = {
+            streamsControllers,
+            dispatch: createDispatch(streamsControllers),
+          };
+          promiseCache.set(reuseKey, {
+            promise: Promise.resolve(fn(req, { ...ctx, sse })),
+            sse,
+          });
+        }
+        const cached = promiseCache.get(reuseKey);
+        cached?.sse.streamsControllers.set(streamKey, {
+          readableStream: this as ReadableStream,
+          controller,
+          reuseKey,
+        });
+        cached?.promise
+          .then(() => controller.close())
+          .finally(() => promiseCache.delete(reuseKey));
+      },
+      cancel() {
+        promiseCache.get(reuseKey)?.sse.streamsControllers.delete(streamKey);
       },
     });
     return new Response(readableStream, {
