ceff6c54be3005d169f014c2e281df0433814576
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2023-08-23 12:44:48 +0200

    fix: error twindConfig.ts

--- twind/twindConfig.ts
+++ twind/twindConfig.ts
@@ -1,4 +1,4 @@
-import { type TwindConfig, defineConfig, install, observe } from https://esm.sh/@twind/core@1.1.3";
+import { type TwindConfig, defineConfig, install, observe } from "https://esm.sh/@twind/core@1.1.3";
 import presetTailwind from "https://esm.sh/@twind/preset-tailwind@1.1.4";
 import presetAutoprefix from "https://esm.sh/@twind/preset-autoprefix@1.0.7";
 import presetTypography from "https://esm.sh/@twind/preset-typography@1.0.7";
