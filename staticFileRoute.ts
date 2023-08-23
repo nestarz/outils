afcb7ff66639315062519bd43dda5fca4b53ef16
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2023-08-23 18:13:33 +0200

    fix: staticFileRoute.ts

--- staticFileRoute.ts
+++ staticFileRoute.ts
@@ -5,24 +5,28 @@ export const config: RouteConfig = {
   routeOverride: "GET@/static/*",
 };
 
-export const handler = async (req: Request) => {
-  const url = new URL(
-    "." + decodeURIComponent(new URL(req.url).pathname),
-    import.meta.url
-  );
-  const resp = await fetch(url);
-  const size = resp?.headers.get("content-length");
-  return resp.status === 200
-    ? new Response(resp.body, {
-        headers: {
-          "content-type": lookup(url.href),
-          ...(size ? { "content-length": String(size) } : {}),
-          "Access-Control-Allow-Credentials": "true",
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          "Access-Control-Allow-Headers": "*",
-          "Cache-Control": "public, max-age=604800, must-revalidate, immutable",
-        },
-      })
-    : new Response(null, { status: 404 });
-};
+export const createHandler =
+  (options: { baseUrl: string; headers?: Record<string, string> }) =>
+  async (req: Request) => {
+    const url = new URL(
+      "." + decodeURIComponent(new URL(req.url).pathname),
+      options.baseUrl
+    );
+    const resp = await fetch(url);
+    const size = resp?.headers.get("content-length");
+    return resp.status === 200
+      ? new Response(resp.body, {
+          headers: {
+            "content-type": lookup(url.href),
+            ...(size ? { "content-length": String(size) } : {}),
+            "Access-Control-Allow-Credentials": "true",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+            "Access-Control-Allow-Headers": "*",
+            "Cache-Control":
+              "public, max-age=604800, must-revalidate, immutable",
+            ...(options.headers ?? {}),
+          },
+        })
+      : new Response(null, { status: 404 });
+  };
