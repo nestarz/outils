91bc0f35d4e49ff369687b742f5be5a75012be6b
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2023-08-23 11:07:19 +0200

    fix: rename basicAuth.ts to createBasicAuth.ts

--- createBasicAuth.ts
+++ createBasicAuth.ts
@@ -1,12 +1,12 @@
 import { basicAuth } from "https://deno.land/x/basic_auth@v1.1.1/mod.ts";
 
 export const createBasicAuth =
-  (username: string, password: string) =>
+  (username: string, password: string, realm = "access") =>
   <T extends Request, U, V>(fn: (arg1: T, arg2: U) => V) =>
   (req: T, ctx: U) => {
     const unauthorized =
       username && password
-        ? basicAuth(req, "access", { [username]: password })
+        ? basicAuth(req, realm, { [username]: password })
         : null;
     return unauthorized ?? fn(req, ctx);
   };
