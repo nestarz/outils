03a84f7b7d46e8c82e2c96d4c4c632477917de87
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2023-09-05 01:37:13 +0200

    fix(cors.ts): pass ctx

--- cors.ts
+++ cors.ts
@@ -5,25 +5,26 @@ function escapeRegExp(string: string) {
 export const createCors =
   ({ hostnames }: { hostnames?: string[] }) =>
   (fn) =>
-  (req: Request) => {
+  (req: Request, ctx) => {
     const isOptions = req.method === "OPTIONS";
     const origin = req.headers.get("Origin")
       ? new URL(req.headers.get("Origin"))
       : null;
-    const allowedOrigin =
-      origin?.hostname.match(/^localhost$/) ||
-      ((origin?.hostname.endsWith("." + hostnames[0]) ||
-        origin?.hostname === hostnames[0]) &&
-        origin?.hostname.match(
-          new RegExp(`(\\.|^)${escapeRegExp(hostnames[0])}$`)
-        ))
+    const allowedOrigin = hostnames
+      ? origin?.hostname.match(/^localhost$/) ||
+        ((origin?.hostname.endsWith("." + hostnames[0]) ||
+          origin?.hostname === hostnames[0]) &&
+          origin?.hostname.match(
+            new RegExp(`(\\.|^)${escapeRegExp(hostnames[0])}$`)
+          ))
         ? origin.origin
-        : null;
+        : null
+      : origin.origin;
     const reqHeaders = (
       req.headers.get("Access-Control-Request-Headers") ?? ""
     ).split(",");
     return Promise.resolve(
-      isOptions ? new Response() : ((...p) => fn(...p))(req)
+      isOptions ? new Response() : ((...p) => fn(...p))(req, ctx)
     )
       .catch(() => new Response(null, { status: 500 }))
       .then((res) =>
