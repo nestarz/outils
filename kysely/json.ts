d93449f7631e2368d26ba7e93e08de662e78e485
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2023-09-09 10:53:25 +0200

    feat(kysely/json.ts): add jsonAgg

--- kysely/json.ts
+++ kysely/json.ts
@@ -1,5 +1,5 @@
-import type { Expression, RawBuilder, Simplify } from "https://esm.sh/kysely@0.26.1?dts";
-import { sql } from "https://esm.sh/kysely@0.26.1?dts";
+import type { Expression, RawBuilder, Simplify } from "https://esm.sh/kysely@0.26.3?dts";
+import { sql } from "https://esm.sh/kysely@0.26.3?dts";
 
 export const jsonObjectFrom = <O>(
   expr: Expression<O>
@@ -10,3 +10,7 @@ export const jsonAggToJsonObjectFrom = <O>(
   expr: Expression<O>
 ): RawBuilder<Simplify<O[]> | null> =>
   sql`(select json_agg(to_json(obj)) from ${expr} as obj)`;
+
+export function jsonAgg<O>(expr: Expression<O>): RawBuilder<Simplify<O>[]> {
+  return sql`json_agg(${expr})`;
+}
