90182301d1bd8f7d1552191907ea84c05f05edcd
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2023-09-10 19:05:13 +0200

    feat(createQueryFunction.ts)

---
+++ kysely/createQueryFunction.ts
@@ -0,0 +1,14 @@
+import type { PoolClient } from "https://deno.land/x/postgres@v0.17.0/mod.ts";
+import type {
+  CompiledQuery,
+  InferResult,
+} from "https://esm.sh/kysely@0.26.3?dts";
+
+const createQueryFunction =
+  <V>(db: Pick<PoolClient, "queryObject">, qb: Kysely<V>) =>
+  <T extends CompiledQuery>(fn: (qb: Kysely<V>) => T) => {
+    const { sql, parameters } = fn(qb);
+    return db.queryObject<InferResult<T>[0]>(sql, parameters as unknown[]);
+  };
+
+export default createQueryFunction;
