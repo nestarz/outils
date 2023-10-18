f1fda40ffe491f4162b913187e98e126144e0223
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2023-10-19 01:33:42 +0200

    feat: add hooks

--- database/createSqliteQueryFunction.ts
+++ database/createSqliteQueryFunction.ts
@@ -6,13 +6,23 @@ import type {
 } from "https://esm.sh/kysely@0.26.3?dts";
 
 const createQueryFunction =
-  <V>(query: DB["queryEntries"], qb: Kysely<V>) =>
+  <V>(
+    query: DB["queryEntries"],
+    qb: Kysely<V>,
+    hooks?: (<X>(...v: any[]) => X)[]
+  ) =>
   <T extends CompiledQuery>(fn: (qb: Kysely<V>) => T) => {
     const { sql, parameters } = fn(qb);
-    return query<InferResult<T>[0]>(
-      sql,
-      parameters as Parameters<DB["queryEntries"]>[1]
+    const value = Promise.resolve(
+      query<InferResult<T>[0]>(
+        sql,
+        parameters as Parameters<DB["queryEntries"]>[1]
+      )
     );
+    return (hooks ?? [])
+      .filter((hook) => typeof hook === "function")
+      .reduce((v, hook) => v.then((v) => hook<typeof v>(v)), value);
   };
+
 
 export default createQueryFunction;
