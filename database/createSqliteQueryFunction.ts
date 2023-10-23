3b6669a50606f88a13161d0ff2c202ae4972b8e6
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2023-10-23 19:13:10 +0200

    Update createSqliteQueryFunction.ts

--- database/createSqliteQueryFunction.ts
+++ database/createSqliteQueryFunction.ts
@@ -1,28 +1,25 @@
-import type { DB } from "https://deno.land/x/sqlite@v3.8/mod.ts";
 import type {
-  Kysely,
   CompiledQuery,
   InferResult,
+  Kysely,
 } from "https://esm.sh/kysely@0.26.3?dts";
 
-const createQueryFunction =
-  <V>(
-    query: DB["queryEntries"],
-    qb: Kysely<V>,
-    hooks?: (<X>(...v: any[]) => X)[]
-  ) =>
-  <T extends CompiledQuery>(fn: (qb: Kysely<V>) => T) => {
-    const { sql, parameters } = fn(qb);
-    const value = Promise.resolve(
-      query<InferResult<T>[0]>(
-        sql,
-        parameters as Parameters<DB["queryEntries"]>[1]
-      )
-    );
-    return (hooks ?? [])
-      .filter((hook) => typeof hook === "function")
-      .reduce((v, hook) => v.then((v) => hook<typeof v>(v)), value);
-  };
-
+const createQueryFunction = <V>(
+  query: <P>(sql: string, params?: unknown[]) => { rows: P },
+  qb: Kysely<V>,
+  hooks?: (<X>(...v: any[]) => X)[],
+) =>
+<T extends CompiledQuery>(fn: (qb: Kysely<V>) => T) => {
+  const { sql, parameters } = fn(qb);
+  const value = Promise.resolve(
+    query<InferResult<T>[0]>(
+      sql,
+      parameters as unknown[],
+    ),
+  );
+  return (hooks ?? [])
+    .filter((hook) => typeof hook === "function")
+    .reduce((v, hook) => v.then((v) => hook<typeof v>(v)), value);
+};
 
 export default createQueryFunction;
