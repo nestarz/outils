7b63ca6a07ac1880d58fb3b272a86d957ef13d95
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2023-11-20 23:25:05 +0100

    Update createRenderPipe.ts

--- createRenderPipe.ts
+++ createRenderPipe.ts
@@ -7,69 +7,70 @@ import { pipe } from "./pipe.ts";
 
 type PipelineFunction<In, Out> = (arg: In) => Out | Promise<Out>;
 
-export const createRenderPipe =
-  <
-    First extends <T>(vnode: T) => any | Promise<any>,
-    Middle extends PipelineFunction<any, any>[],
-    Last extends PipelineFunction<
-      any,
-      BodyInit | null | Promise<BodyInit | null>
-    >
-  >(
-    firstFn: First,
-    ...fns: [...Middle, Last]
-  ) =>
-  (
-    route: {
-      default: (<T>(props: Record<string, unknown>) => T) | Handlers["GET"];
-      handler: Handlers;
-      config?: { routeOverride?: string };
-    },
-    config?: {
-      responseInit?: ResponseInit;
-      Layout?: { default: ReturnType<typeof defineLayout> };
-    }
-  ) =>
-  (req: Request, ctx: HandlerContext, matcher?: Record<string, string>) => {
-    const url = new URL(req.url);
-    const newCtx = matcher ? { ...ctx, params: matcher, url } : { ...ctx, url };
-    const props = { url, ctx: newCtx };
+export const createRenderPipe = <
+  First extends <T>(vnode: T) => any | Promise<any>,
+  Middle extends PipelineFunction<any, any>[],
+  Last extends PipelineFunction<
+    any,
+    BodyInit | null | Promise<BodyInit | null>
+  >,
+>(
+  firstFn: First,
+  ...fns: [...Middle, Last]
+) =>
+(
+  route: {
+    default: (<T>(props: Record<string, unknown>) => T) | Handlers["GET"];
+    handler: Handlers;
+    config?: { routeOverride?: string };
+  },
+  config?: {
+    responseInit?: ResponseInit;
+    Layout?: { default: ReturnType<typeof defineLayout> };
+  },
+) =>
+(req: Request, ctx: HandlerContext, matcher?: Record<string, string>) => {
+  const url = new URL(req.url);
+  const newCtx = matcher ? { ...ctx, params: matcher, url } : { ...ctx, url };
+  const props = { url, ctx: newCtx };
 
-    const vNodePipe = pipe(
-      firstFn,
-      ...fns as any[],
-      (result: Response | BodyInit | null) =>
-        result instanceof Response
-          ? result
-          : new Response(result, {
-              headers: {
-                "content-type": "text/html; charset=utf-8",
-              },
-              ...(config?.responseInit ?? {}),
-            })
-    );
+  const vNodePipe = pipe(
+    firstFn,
+    ...(fns as any[]),
+    (result: Response | BodyInit | null) =>
+      result instanceof Response ? result : new Response(result, {
+        headers: {
+          "content-type": "text/html; charset=utf-8",
+        },
+        ...(config?.responseInit ?? {}),
+      }),
+  );
 
   const render = route.default
     ? pipe(
       (data: unknown) =>
         route.handler?.GET ? [{ ...props, data }] : [req, newCtx],
       ([p1, p2]) =>
-        Promise.resolve().then(() => route.default?.(p1, p2))
+        Promise.resolve()
+          .then(() => route.default?.(p1, p2))
           .catch((err) => (console.error(err), Promise.reject(err))),
       (node) => [node, { ...newCtx, Component: () => node }],
       ([node, ctx]) =>
         node instanceof Response ? node : vNodePipe(
-          Promise.resolve().then(() => config?.Layout?.default(req, ctx) ?? node).catch(
-            (err) => (console.error(err), Promise.reject(err)),
-          ),
+          Promise.resolve()
+            .then(() => config?.Layout?.default(req, ctx) ?? node)
+            .catch((err) => (console.error(err), Promise.reject(err))),
         ),
     )
     : undefined;
-    
-    return (
-      route.handler?.[req.method]?.(req, { ...newCtx, render, vNodePipe }) ??
-      (req.method === "GET" ? render?.() : new Response(null, { status: 404 }))
-    );
-  };
+
+  const onfullfiled = () =>
+    route.handler?.[req.method]?.(req, { ...newCtx, render, vNodePipe }) ??
+      (req.method === "GET" ? render?.() : new Response(null, { status: 404 }));
+
+  return Promise.resolve()
+    .then(onfullfiled)
+    .catch((err) => (console.error(err), Promise.reject(err)));
+};
 
 export default createRenderPipe;
