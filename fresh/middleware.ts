66bb2e50a208cedecff47277b27a97275099aafb
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2023-11-22 01:59:24 +0100

    Update middleware.ts

--- fresh/middleware.ts
+++ fresh/middleware.ts
@@ -1,32 +1,30 @@
-import type { ConnInfo } from "https://deno.land/std@0.190.0/http/server.ts";
-import type { MiddlewareHandlerContext } from "https://deno.land/x/fresh@1.1.6/server.ts";
+import type {
+  Handler,
+  MiddlewareHandler,
+  MiddlewareHandlerContext,
+} from "https://deno.land/x/fresh@1.5.4/server.ts";
 
-export interface Middleware {
-  (request: Request, ctx: MiddlewareHandlerContext):
-    | Response
-    | Promise<Response>;
-}
-
-export const middleware =
-  (...fns: Middleware[]) =>
-  async (
-    req: Request,
-    conn: ConnInfo,
-    params: Record<string, string>
-  ): Promise<Response> => {
-    const destination = "route" as MiddlewareHandlerContext["destination"];
-    const ctx = {
-      remoteAddr: conn.remoteAddr,
-      localAddr: conn.localAddr,
-      params,
-      state: {},
-      destination,
-    };
-    const createNext = (i: number): MiddlewareHandlerContext => ({
-      ...ctx,
-      next: async () => await fns[i](req, createNext(i + 1)),
-    });
-    return await fns[0]?.(req, createNext(1));
+export const middleware = (...fns: [...MiddlewareHandler[], Handler]) =>
+async (
+  req: Request,
+  conn: Deno.ServeHandlerInfo,
+  params: Record<string, string>,
+): Promise<Response> => {
+  const destination = "route" as MiddlewareHandlerContext["destination"];
+  const ctx = {
+    remoteAddr: conn.remoteAddr,
+    localAddr: { transport: "tcp" } as Deno.NetAddr,
+    hostname: "",
+    params,
+    state: {},
+    destination,
   };
+  const createNext = (i: number): MiddlewareHandlerContext => ({
+    ...ctx,
+    next: async () =>
+      await (fns[i] as MiddlewareHandler)(req, createNext(i + 1)),
+  });
+  return await (fns[0] as MiddlewareHandler)?.(req, createNext(1));
+};
 
 export default middleware;
