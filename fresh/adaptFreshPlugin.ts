8a91e798655b473fa095fe69dd71af0ddd682dc1
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2023-11-22 22:44:01 +0100

    Update adaptFreshPlugin.ts

--- fresh/adaptFreshPlugin.ts
+++ fresh/adaptFreshPlugin.ts
@@ -1,9 +1,14 @@
 import type { Plugin } from "https://deno.land/x/fresh@1.5.4/src/server/types.ts";
 
+type JSXElement<T = any> = { type: T; props: any; key: string };
 export const adaptFreshPlugin = <T,>(plugin: Plugin<T>) => ({
   middlewares: (plugin.middlewares ?? []).flatMap((d) => d.middleware.handler),
   routes: (plugin.routes ?? []).map((route) => ({
-    default: route.component,
+    default: route
+      .component as unknown as ((
+        req: Request,
+        context?: any,
+      ) => Promise<JSXElement<any>>),
     handler: route.handler,
     config: { routeOverride: route.path },
   })),
