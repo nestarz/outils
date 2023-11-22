f2d8dbd2386aa197388dfb56a28616ff5f58309e
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2023-11-22 01:58:35 +0100

    fix: createRenderPipe.ts

--- createRenderPipe.ts
+++ createRenderPipe.ts
@@ -1,76 +1,82 @@
-import type { defineLayout } from "$fresh/src/server/defines.ts";
 import type {
+  AsyncLayout,
   HandlerContext,
   Handlers,
-} from "https://deno.land/x/fresh@1.4.2/server.ts";
-import { pipe } from "./pipe.ts";
+} from "https://deno.land/x/fresh@1.5.4/src/server/types.ts";
 
-type PipelineFunction<In, Out> = (arg: In) => Out | Promise<Out>;
-
-export const createRenderPipe = <
-  First extends <T>(vnode: T) => any | Promise<any>,
-  Middle extends PipelineFunction<any, any>[],
-  Last extends PipelineFunction<
-    any,
-    BodyInit | null | Promise<BodyInit | null>
-  >,
->(
-  firstFn: First,
-  ...fns: [...Middle, Last]
-) =>
-(
-  route: {
-    default: (<T>(props: Record<string, unknown>) => T) | Handlers["GET"];
-    handler: Handlers;
-    config?: { routeOverride?: string };
-  },
-  config?: {
-    responseInit?: ResponseInit;
-    Layout?: { default: ReturnType<typeof defineLayout> };
-  },
-) =>
-(req: Request, ctx: HandlerContext, matcher?: Record<string, string>) => {
-  const url = new URL(req.url);
-  const newCtx = matcher ? { ...ctx, params: matcher, url } : { ...ctx, url };
-  const props = { url, ctx: newCtx };
-
-  const vNodePipe = pipe(
-    firstFn,
-    ...(fns as any[]),
-    (result: Response | BodyInit | null) =>
-      result instanceof Response ? result : new Response(result, {
-        headers: {
-          "content-type": "text/html; charset=utf-8",
-        },
-        ...(config?.responseInit ?? {}),
-      }),
-  );
-
-  const render = route.default
-    ? pipe(
-      (data: unknown) =>
-        route.handler?.GET ? [{ ...props, data }] : [req, newCtx],
-      ([p1, p2]) =>
-        Promise.resolve()
-          .then(() => route.default?.(p1, p2))
-          .catch((err) => (console.error(err), Promise.reject(err))),
-      (node) => [node, { ...newCtx, Component: () => node }],
-      ([node, ctx]) =>
-        node instanceof Response ? node : vNodePipe(
-          Promise.resolve()
-            .then(() => config?.Layout?.default(req, ctx) ?? node)
-            .catch((err) => (console.error(err), Promise.reject(err))),
-        ),
-    )
-    : undefined;
-
-  const onfullfiled = () =>
-    route.handler?.[req.method]?.(req, { ...newCtx, render, vNodePipe }) ??
-      (req.method === "GET" ? render?.() : new Response(null, { status: 404 }));
-
-  return Promise.resolve()
-    .then(onfullfiled)
-    .catch((err) => (console.error(err), Promise.reject(err)));
+type JSXElement = { type: any; props: any; key: string };
+type RequestHandler<VNode extends JSXElement = JSXElement> = (
+  req: Request,
+  ctx: HandlerContext,
+) => Promise<VNode>;
+type PropHandler<VNode extends JSXElement = JSXElement> = (
+  props: Record<string, unknown>,
+) => Promise<VNode>;
+export type RouteConfig<VNode extends JSXElement = JSXElement> = {
+  default: RequestHandler<VNode> | PropHandler<VNode>;
+  handler?: Handlers;
+  config?: { routeOverride?: string };
 };
+
+export const createRenderPipe =
+  // deno-lint-ignore no-explicit-any
+  <VNode extends JSXElement>(
+    virtualNodePipe: (
+      vn: VNode,
+    ) => Promise<BodyInit | null | undefined | Response>,
+  ) =>
+  (
+    route: RouteConfig<VNode>,
+    config?: { responseInit?: ResponseInit; Layout?: { default: AsyncLayout } },
+  ) =>
+  (req: Request, rawCtx: HandlerContext, matcher?: Record<string, string>) => {
+    const url = new URL(req.url);
+    const ctx = {
+      ...rawCtx,
+      params: matcher ?? {},
+      url,
+      route: route.config?.routeOverride ?? null!,
+      data: null!,
+      virtualNodePipe,
+    };
+
+    const render = route.default
+      ? (data?: Record<string, unknown>) =>
+        Promise.resolve(
+          route.handler?.GET
+            ? (route.default as RequestHandler<VNode>)?.(req, ctx)
+            : (route.default as PropHandler<VNode>)?.({ url, ctx, data }),
+        )
+          .then((node) =>
+            node instanceof Response
+              ? node
+              : !node
+              ? Promise.reject("Missing Response")
+              : Promise.resolve({ ...ctx, Component: () => node })
+                .then((ctx) =>
+                  (config?.Layout?.default(req, ctx) as Promise<typeof node>) ??
+                    node
+                )
+                .then(virtualNodePipe)
+                .then(
+                  (result) =>
+                    result instanceof Response ? result : new Response(result, {
+                      headers: { "content-type": "text/html; charset=utf-8" },
+                    }),
+                )
+          )
+      : undefined;
+
+    return Promise.resolve({ ...ctx, render: render! })
+      .then((ctx) =>
+        route.handler?.[req.method as keyof Handlers]?.(req, ctx) ??
+            req.method === "GET"
+          ? render?.()
+          : new Response(null, { status: 404 })
+      )
+      .then((response) => response ?? Promise.reject("Missing Response"))
+      .catch((err) => (console.error(err), Promise.reject(err)))
+      .catch(() => new Response(null, { status: 500 }));
+  };
 
 export default createRenderPipe;
