219a0a702ef00d912f0d9fc1166b61236979d265
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2023-11-22 15:26:35 +0100

    Update sqliteMiddleware.ts

--- sqliteMiddleware.ts
+++ sqliteMiddleware.ts
@@ -1,13 +1,24 @@
 import type { MiddlewareHandlerContext } from "https://deno.land/x/fresh@1.5.2/server.ts";
-import type { DB } from "https://deno.land/x/sqlite@v3.8/mod.ts";
+import type {
+  DB,
+  QueryParameterSet,
+  RowObject,
+} from "https://deno.land/x/sqlite@v3.8/mod.ts";
 
 import { Kysely, SqliteDialect } from "https://esm.sh/kysely@0.26.3?dts";
 import sqliteGenTypes from "https://deno.land/x/outils@0.0.87/database/generateSqliteTypes.ts";
 import createQueryFunction from "https://deno.land/x/outils@0.0.87/database/createSqliteQueryFunction.ts";
 import deserializeNestedJSON from "https://deno.land/x/outils@0.0.87/deserializeNestedJSON.ts";
 
+interface DBClassic {
+  query: (
+    query: string,
+    values?: QueryParameterSet,
+  ) => Promise<RowObject[] | undefined>;
+}
+
 export type SqliteMiddlewareState<Schema> = {
-  db: DB;
+  db: DB | DBClassic;
   qb: Kysely<Schema>;
   clientQuery: ReturnType<typeof createQueryFunction<Schema>>;
 };
@@ -26,13 +37,11 @@ export const createSqliteMiddleware = <Schema>({
   return {
     handler: async (
       _req: Request,
-      ctx: MiddlewareHandlerContext<SqliteMiddlewareState<Schema>>
+      ctx: MiddlewareHandlerContext<SqliteMiddlewareState<Schema>>,
     ) => {
       ctx.state.db = database;
-      const dbQuery =
-        ctx.state.db.queryEntries ??
-        ((ctx.state.db as unknown as { query: unknown })
-          .query as unknown as typeof ctx.state.db.queryEntries);
+      const dbQuery = ctx.state.db.queryEntries ??
+        ctx.state.db.query;
       const qb = new Kysely<Schema>({
         dialect: new SqliteDialect(null!),
       });
