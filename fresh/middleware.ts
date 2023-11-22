19cba0f2c654d16ed8600b1e36bf6caf97f131c8
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2023-11-22 18:14:17 +0100

    Update middleware.ts

--- fresh/middleware.ts
+++ fresh/middleware.ts
@@ -4,27 +4,30 @@ import type {
   MiddlewareHandlerContext,
 } from "https://deno.land/x/fresh@1.5.4/server.ts";
 
-export const middleware = (...fns: [...MiddlewareHandler[], Handler]) =>
-async (
-  req: Request,
-  conn: Deno.ServeHandlerInfo,
-  params: Record<string, string>,
-): Promise<Response> => {
-  const destination = "route" as MiddlewareHandlerContext["destination"];
-  const ctx = {
-    remoteAddr: conn.remoteAddr,
-    localAddr: { transport: "tcp" } as Deno.NetAddr,
-    hostname: "",
-    params,
-    state: {},
-    destination,
+export const middleware =
+  // deno-lint-ignore no-explicit-any
+  (...fns: [...MiddlewareHandler<any>[], Handler]) =>
+  async (
+    req: Request,
+    conn: Deno.ServeHandlerInfo,
+    params: Record<string, string>,
+  ): Promise<Response> => {
+    const destination = "route" as MiddlewareHandlerContext["destination"];
+    const ctx = {
+      remoteAddr: conn.remoteAddr,
+      localAddr: { transport: "tcp" } as Deno.NetAddr,
+      hostname: "",
+      params,
+      state: {},
+      destination,
+    };
+    // deno-lint-ignore no-explicit-any
+    const createNext = (i: number): MiddlewareHandlerContext<any> => ({
+      ...ctx,
+      next: async () =>
+        await (fns[i] as MiddlewareHandler)(req, createNext(i + 1)),
+    });
+    return await (fns[0] as MiddlewareHandler)?.(req, createNext(1));
   };
-  const createNext = (i: number): MiddlewareHandlerContext => ({
-    ...ctx,
-    next: async () =>
-      await (fns[i] as MiddlewareHandler)(req, createNext(i + 1)),
-  });
-  return await (fns[0] as MiddlewareHandler)?.(req, createNext(1));
-};
 
 export default middleware;
