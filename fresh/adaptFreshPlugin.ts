7a11d24e76bfcd99fd698cccfaf1a316b9de84f7
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2023-11-22 02:00:36 +0100

    Create adaptFreshPlugin.ts

---
+++ fresh/adaptFreshPlugin.ts
@@ -0,0 +1,12 @@
+import type { Plugin } from "https://deno.land/x/fresh@1.5.4/src/server/types.ts";
+
+export const adaptFreshPlugin = (plugin: Plugin) => ({
+  middlewares: (plugin.middlewares ?? []).map((d) => d.middleware.handler),
+  routes: (plugin.routes ?? []).map((route) => ({
+    default: route.component,
+    handler: route.handler,
+    config: { routeOverride: route.path },
+  })),
+});
+
+export default adaptFreshPlugin;
