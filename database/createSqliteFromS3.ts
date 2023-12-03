af028d66b5d7297dbd3e6d718c7e1a8504ff8ebe
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2023-12-03 14:04:05 +0100

    Update createSqliteFromS3.ts

--- database/createSqliteFromS3.ts
+++ database/createSqliteFromS3.ts
@@ -1,11 +1,14 @@
-import type { S3Client } from "https://deno.land/x/s3_lite_client@0.6.1/mod.ts";
+import type { S3Client } from "https://deno.land/x/s3_lite_client@0.6.2/mod.ts";
 import sqliteMemorySync from "../sqliteMemorySync.ts";
 
-export const createSqliteFromS3 = (filename: string, s3Client: S3Client) =>
+export const createSqliteFromS3 = (
+  filename: string,
+  s3Client: Pick<S3Client, "getObject" | "putObject" | "statObject">
+) =>
   sqliteMemorySync(
     () => s3Client.getObject(filename).then((r) => r.arrayBuffer()),
     (buffer) => s3Client.putObject(filename, buffer).then(() => true),
-    () => s3Client.statObject(filename).then((r) => r.etag),
+    () => s3Client.statObject(filename).then((r) => r.etag)
   );
 
 export default createSqliteFromS3;
