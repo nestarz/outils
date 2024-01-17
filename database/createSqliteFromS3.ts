9263e51ae624f06cb4efb48ea9614c64cbc68ead
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2024-01-17 15:30:15 +0100

    Update createSqliteFromS3.ts

--- database/createSqliteFromS3.ts
+++ database/createSqliteFromS3.ts
@@ -1,14 +1,43 @@
 import type { S3Client } from "https://deno.land/x/s3_lite_client@0.6.2/mod.ts";
 import sqliteMemorySync from "../sqliteMemorySync.ts";
 
+const createId = ((i=0) => () => i++); // prettier-ignore
+const getObjectTimerId = createId();
 export const createSqliteFromS3 = (
   filename: string,
   s3Client: Pick<S3Client, "getObject" | "putObject" | "statObject">
 ) =>
   sqliteMemorySync(
-    () => s3Client.getObject(filename).then((r) => r.arrayBuffer()),
-    (buffer) => s3Client.putObject(filename, buffer).then(() => true),
-    () => s3Client.statObject(filename).then((r) => r.etag)
+    () => {
+      const id = `[s3lite:getObject:${getObjectTimerId()}]`;
+      return (
+        console.time(id),
+        s3Client
+          .getObject(filename)
+          .then((r) => r.arrayBuffer())
+          .finally(() => console.timeEnd(id))
+      );
+    },
+    (buffer) => {
+      const id = `[s3lite:putObject:${getObjectTimerId()}]`;
+      return (
+        console.time(id),
+        s3Client
+          .putObject(filename, buffer)
+          .then(() => true)
+          .finally(() => console.timeEnd(id))
+      );
+    },
+    () => {
+      const id = `[s3lite:statObject:${getObjectTimerId()}]`;
+      return (
+        console.time(id),
+        s3Client
+          .statObject(filename)
+          .then((r) => r.etag)
+          .finally(() => console.timeEnd(id))
+      );
+    }
   );
 
 export default createSqliteFromS3;
