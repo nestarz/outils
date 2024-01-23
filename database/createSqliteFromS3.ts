ca9a3f633f56dc37ecf9fbccb13b13d6f3723665
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2024-01-23 14:37:43 +0100

    Update createSqliteFromS3.ts

--- database/createSqliteFromS3.ts
+++ database/createSqliteFromS3.ts
@@ -1,43 +1,48 @@
 import type { S3Client } from "https://deno.land/x/s3_lite_client@0.6.2/mod.ts";
 import sqliteMemorySync from "../sqliteMemorySync.ts";
 
-const createId = ((i=0) => () => i++); // prettier-ignore
+const createId = (i = 0) => () => i++; // prettier-ignore
 const getObjectTimerId = createId();
+
 export const createSqliteFromS3 = (
   filename: string,
-  s3Client: Pick<S3Client, "getObject" | "putObject" | "statObject">
+  s3Client: Pick<S3Client, "getObject" | "putObject" | "statObject">,
+  createAcquireLock?: (
+    filename: string,
+  ) => (<T>(fn: () => T) => Promise<T>) | null,
 ) =>
   sqliteMemorySync(
     () => {
       const id = `[s3lite:getObject:${getObjectTimerId()}]`;
       return (
         console.time(id),
-        s3Client
-          .getObject(filename)
-          .then((r) => r.arrayBuffer())
-          .finally(() => console.timeEnd(id))
+          s3Client
+            .getObject(filename)
+            .then((r) => r.arrayBuffer())
+            .finally(() => console.timeEnd(id))
       );
     },
     (buffer) => {
       const id = `[s3lite:putObject:${getObjectTimerId()}]`;
       return (
         console.time(id),
-        s3Client
-          .putObject(filename, buffer)
-          .then(() => true)
-          .finally(() => console.timeEnd(id))
+          s3Client
+            .putObject(filename, buffer)
+            .then(() => true)
+            .finally(() => console.timeEnd(id))
       );
     },
     () => {
       const id = `[s3lite:statObject:${getObjectTimerId()}]`;
       return (
         console.time(id),
-        s3Client
-          .statObject(filename)
-          .then((r) => r.etag)
-          .finally(() => console.timeEnd(id))
+          s3Client
+            .statObject(filename)
+            .then((r) => r.etag)
+            .finally(() => console.timeEnd(id))
       );
-    }
+    },
+    createAcquireLock?.(filename),
   );
 
 export default createSqliteFromS3;
